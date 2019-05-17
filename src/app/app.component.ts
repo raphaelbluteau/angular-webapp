373fb36626c2eb50338a7ee16e5c2fbce7fc7feb
Raphael Macedo de Carvalho <raphaelbluteau@gmail.com>
2019-05-16 23:35:37 -0300

    Adding projects.json asset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ProjectModel } from './project.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'webapp';
+
+  constructor(public http: HttpClient) {}
+
+  ngOnInit(): void {
+    
+  }
 }
---
+++ src/app/project.model.ts
@@ -0,0 +1,7 @@
+export class ProjectModel {
+    id: number;
+    name: string;
+    description: string;
+    imageUrl: string;
+    imageAlt: string
+}
